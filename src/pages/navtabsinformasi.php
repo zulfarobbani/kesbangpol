8ba3cc4ce6c19cc5df793ae3b8b75bf98899712d
unknown <minaryanshofa1@gmail.com>
2021-06-28 10:26:27 +0700

    revisi navtabs informasi

--- src/pages/navtabsinformasi.php
+++ src/pages/navtabsinformasi.php
@@ -10,26 +10,26 @@
   }
 </style>
 <div class="container px-0 py-3 rounded justify-content-start">
-  <div class="row py-1" id="bgprofile">
-    <div class="col-sm-2 p-2" >
+  <div class="row py-1 d-flex justify-content-center" id="bgprofile">
+    <div class="col-sm-3 p-2" >
       <a class="nav-link p-0 text-center position-relative top-50 start-50 translate-middle fw-bold" aria-current="page" href="\forum-umum">
         <span class="material-icons-outlined align-middle">article</span>
         <span>Berita</span>
       </a>
     </div>
-    <div class="col-sm-2 px-2">
+    <div class="col-sm-3 px-2">
       <a class="nav-link p-0 text-center position-relative top-50 start-50 translate-middle fw-bold" href="\forum-private">
         <span class="material-icons-outlined align-middle">campaign</span>
         <span>Pengumuman</span>
       </a>
     </div>
-    <div class="col-sm-2 p-2" >
-      <a class="nav-link p-0 text-center position-relative top-50 start-50 translate-middle fw-bold" aria-current="page" href="\forum-umum">
+    <div class="col-sm-3 p-2" >
+      <a class="nav-link p-0 text-center position-relative top-50 start-50 translate-middle fw-bold" aria-current="page" href="\agenda">
         <span class="material-icons-outlined align-middle">event_note</span>
         <span>Agenda</span>
       </a>
     </div>
-    <div class="col-sm-2 px-2">
+    <div class="col-sm-3 px-2">
       <a class="nav-link p-0 text-center position-relative top-50 start-50 translate-middle fw-bold" href="\forum-private">
         <span class="material-icons-outlined align-middle">photo_library</span>
         <span>Galeri</span>
